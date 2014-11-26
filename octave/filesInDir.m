7a24f9803843a2a9b672af614aa65c7276284236
Shawn Cook <cooksha@yahoo-inc.com>
2014-11-25 20:26:55 -0800

    Add stuff

---
+++ octave/filesInDir.m
@@ -0,0 +1,16 @@
+dirList = dir('../data');
+
+notCwd = ~strcmp('.', {dirList.name});
+dirList = dirList(notCwd);
+
+notPwd = ~strcmp('..', {dirList.name});
+dirList = dirList(notPwd);
+
+symbols = {dirList.name};
+
+#isDir=[dirList.isdir];
+#fileNames={dirList(isDir).name};
+#notCwd=~strcmp('.', fileNames);
+#notPwd=~strcmp('..', fileNames);
+#fileNames=fileNames(notCwd);
+#fileNames={fileNames(notPwd)};
